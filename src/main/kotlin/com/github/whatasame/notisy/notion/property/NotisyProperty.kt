5472f1eba2ba747d0267fca8d05167014852691b
whatasame <whatasame@gmail.com>
2023-05-30 02:10:49 +0900

    feat: Template DB 속성

---
+++ src/main/kotlin/com/github/whatasame/notisy/notion/property/NotisyProperty.kt
@@ -0,0 +1,11 @@
+package com.github.whatasame.notisy.notion.property
+
+enum class NotisyProperty(val value: String) {
+
+    STATUS("Status"),
+    TITLE("Title"),
+    CREATED_TIME("Created time"),
+    LAST_EDITED_TIME("Last edited time"),
+    POST_TIME("Post time"),
+    POST_ID("Post ID")
+}
