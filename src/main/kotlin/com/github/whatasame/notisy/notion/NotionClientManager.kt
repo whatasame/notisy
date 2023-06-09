a611c65ac04e6db88acad7f5789a623c04834795
whatasame <whatasame@gmail.com>
2023-06-09 22:19:54 +0900

    fix: NotionClient를 획득하는 방법 변경
    - 변경 전: 싱글톤 패턴으로 하나의 NotionClient를 공유하여 사용
    - 문제점: 여러 쓰레드에서 NotionClient를 이용하여 Http 요청을 보낼 때 Http 정보에 대한 race condition 발생 가능

--- src/main/kotlin/com/github/whatasame/notisy/notion/NotionClientManager.kt
+++ src/main/kotlin/com/github/whatasame/notisy/notion/NotionClientManager.kt
@@ -6,17 +6,7 @@ import notion.api.v1.NotionClient
 
 object NotionClientManager {
 
-    private var client: NotionClient? = null
-
     fun getClient(): NotionClient {
-        if (client == null) {
-            client = refreshClient()
-        }
-
-        return client!!
-    }
-
-    fun refreshClient(): NotionClient {
         val token: String = KeyManager().readKey(NOTION_TOKEN)
 
         return NotionClient(token)
