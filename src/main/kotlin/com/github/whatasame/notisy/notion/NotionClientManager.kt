e8233171b5c5e84845ec9824e99587da4ff18f5f
whatasame <whatasame@gmail.com>
2023-06-11 04:18:19 +0900

    fix: PATCH 에러 수정
    https://github.com/seratch/notion-sdk-jvm#oauth-support

--- src/main/kotlin/com/github/whatasame/notisy/notion/NotionClientManager.kt
+++ src/main/kotlin/com/github/whatasame/notisy/notion/NotionClientManager.kt
@@ -3,13 +3,17 @@ package com.github.whatasame.notisy.notion
 import com.github.whatasame.notisy.key.Key.NOTION_TOKEN
 import com.github.whatasame.notisy.key.KeyManager
 import notion.api.v1.NotionClient
+import notion.api.v1.http.OkHttp4Client
 
 object NotionClientManager {
 
     fun getClient(): NotionClient {
         val token: String = KeyManager().readKey(NOTION_TOKEN)
 
-        return NotionClient(token)
+        return NotionClient(
+            token = token,
+            httpClient = OkHttp4Client()
+        )
     }
 
 }
